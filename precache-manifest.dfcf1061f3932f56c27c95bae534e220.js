860a80567f3a0bcb16335d8ea6c79605d000cb5e
kv kishore <kanala.kishore@zensar.com>
2024-05-15 15:35:13 +0530

    Updates

---
+++ precache-manifest.dfcf1061f3932f56c27c95bae534e220.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d5d62c5c75b295da87f2e2501a6c82",
+    "url": "/sample/index.html"
+  },
+  {
+    "revision": "517b79636ce06bebcbea",
+    "url": "/sample/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "bf4f4f8fc7648a6defa5",
+    "url": "/sample/static/css/main.10a72923.chunk.css"
+  },
+  {
+    "revision": "517b79636ce06bebcbea",
+    "url": "/sample/static/js/2.06be1ed3.chunk.js"
+  },
+  {
+    "revision": "bf4f4f8fc7648a6defa5",
+    "url": "/sample/static/js/main.1551857a.chunk.js"
+  },
+  {
+    "revision": "4c56d8bb3462e6b62d6b",
+    "url": "/sample/static/js/runtime~main.60cb1095.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/sample/static/media/logo.9f7e506d.svg"
+  }
+]);
